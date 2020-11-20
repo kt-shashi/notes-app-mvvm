ba40e5bd673fd305030f72b0cbb4f6c7adaa7ebc
shashiofficial <shashiofficial10@gmail.com>
2020-11-20 18:40:32 +0530

    Added: NoteViewModel

---
+++ app/src/main/java/com/shashi/notesappmvvm/NoteViewModel.kt
@@ -0,0 +1,17 @@
+package com.shashi.notesappmvvm
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    val allNotes: LiveData<List<Note>>
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNodeDao()
+        val repository = NoteRepository(dao)
+
+        allNotes = repository.allNotes
+    }
+}
